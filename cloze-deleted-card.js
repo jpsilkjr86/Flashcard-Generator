137c79e5c163eaff00a7a129640bf5897b6faf42
John Silkey <jpsilkjr86@gmail.com>
2017-07-01 12:03:02 -0700

    added some paramter validation within the ClozedDeletedCard constructor in case the front-end fails to validate correctly

--- cloze-deleted-card.js
+++ cloze-deleted-card.js
@@ -1,6 +1,9 @@
 // imports fs
 var fs = require('fs');
 
+// imports BasicCard in case constructor is called incorrectly.
+var BasicCard = require('./basic-card.js');
+
 // declares ClozeDeletedCard constructor
 var ClozeDeletedCard = function(fullAnswer, omittedPart) {
 	// checks right away to see if it's not already an instance of the constructor
@@ -10,6 +13,15 @@ var ClozeDeletedCard = function(fullAnswer, omittedPart) {
 
 	this.fullAnswer = fullAnswer;
 	this.omittedPart = omittedPart;
+
+	// input validation within the constructor in case it is not carried 
+	// out on the front end. If omittedPart is not a subset of fullText...
+	if (this.fullAnswer.search(this.omittedPart) === -1) {
+		console.log('Error: Not a valid cloze-deleted card.'
+			+ '\nThe cloze is not a subset of the full text.'
+			+ '\nCreating new "Basic Flashcard"...\n');
+		return new BasicCard(fullAnswer, omittedPart);
+	}
 
 	// front is the omittedPart of fullAnswer replaced with '...'
 	// back is always the same as fullAnswer
